140b8687b313ea2ad891498ca64f564e4b6eb1fe
Yogesh <yogeshkumar86077@gmail.com>
2024-09-11 15:07:16 +0530

    server

--- server/server.js
+++ server/server.js
@@ -93,8 +93,8 @@ const app = express();
 
 // CORS configuration
 const corsOptions = {
-  // origin: 'https://datafrontend.saumic.com', // Replace with your frontend domain
-  origin: 'http://localhost:3000', //   Replace with your frontend domain
+  origin: 'https://datafrontend.saumic.com', // Replace with your frontend domain
+  // origin: 'http://localhost:3000', //   Replace with your frontend domain
 
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization'],
